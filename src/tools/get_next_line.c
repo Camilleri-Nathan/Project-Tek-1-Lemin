c514fec0820eb60501239968bd04ce9909fcf795
Joubert Sacha <Joubert_S@desktop-ps7pru0.home>
2018-03-12 18:05:25 +0100

    sort file

--- src/tools/get_next_line.c
+++ src/tools/get_next_line.c
@@ -1,84 +1,102 @@
 /*
 ** EPITECH PROJECT, 2018
-** get_next_line.c
+** get_next_line1.c
 ** File description:
-** contain get_next_line.c
+** gnl
 */
 
-#include <stdio.h>
-#include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include "get_next_line.h"
 
-char	*my_realloc(char *line, int nb)
+static int	my_strlen(char const *str, int pos)
 {
-	char *tmp = line;
-	int carac = 0;
+	int	count = pos;
 
-	line = malloc(sizeof(char *) * ((READ_SIZE * nb) + 1));
-	if (line == NULL) {
-		return (NULL);
-	}
-	while (tmp[carac] != '\0') {
-		line[carac] = tmp[carac];
-		carac += 1;
-	}
-	free(tmp);
-	return (line);
+	while (str != NULL && (str[count] != '\n' && str[count] != '\0'))
+		count++;
+	return (count);
 }
 
-int	change_line(int *carac, int *readed, char *buffer, char *line)
+static char	*realloc_ret(char *buffer, char *ret, int *count_buf)
 {
-	int size = 0;
+	char	*tmp = malloc(sizeof(char) *
+			(my_strlen(buffer, *count_buf)
+			+ my_strlen(ret, 0) + 1));
+	int	count = 0;
 
-	while (size != *readed) {
-		if (buffer[size] == '\n') {
-			line[*carac] = '\0';
-			size += 1;
-			return (size);
+	if (tmp == NULL) {
+		free(ret);
+		return (ERROR);
+	}
+	if (ret != NULL) {
+		while (ret[count] != '\0') {
+			tmp[count] = ret[count];
+			count++;
 		}
-		line[*carac] = buffer[size];
-		*carac += 1;
-		size += 1;
 	}
-	line[*carac] = '\0';
-	return (size);
+	while (buffer[*count_buf] != '\0' && buffer[*count_buf] != '\n')
+		tmp[count++] = buffer[(*count_buf)++];
+	tmp[count] = '\0';
+	free(ret);
+	return (tmp);
 }
 
-char	*make_line(int fd, char *buffer, int *size, int *readed)
+static int	read_file(int fd, char *buffer,
+			int *count_buf, int *current_file)
 {
-	char *line = NULL;
-	int nb = 1;
-	int carac = 0;
+	*count_buf = 0;
+	if ((*current_file = read(fd, buffer, READ_SIZE)) < 0)
+		return (FAIL);
+	buffer[*current_file] = '\0';
+	return (*current_file);
+}
 
-	line = malloc(sizeof(char *) * (READ_SIZE + 1));
-	while (1) {
-		*size = change_line(&carac, readed, buffer, line);
-		if (buffer[*size - 1] == '\n')
-			return (line);
-		*size = 0;
-		*readed = read(fd, buffer, READ_SIZE);
-		if (*readed == 0)
-			return (line);
-		nb += 1;
-		line = my_realloc(line, nb);
+static int	begin_read(char *buffer, int *current_file, int fd, char **ret)
+{
+	int		res = 0;
+	static int	count_buf = 0;
+
+	if (buffer[count_buf] == '\0' || count_buf != READ_SIZE ||
+	count_buf != 0) {
+		*ret = realloc_ret(buffer, *ret, &count_buf);
+		if (buffer[count_buf] == '\n')
+			return (++count_buf, CONTINUE);
 	}
+	while ((res = read_file(fd, buffer, &count_buf, current_file)) > 0) {
+		if ((*ret = realloc_ret(buffer, *ret, &count_buf)) == ERROR)
+			return (FAIL);
+		if (buffer[count_buf] == '\n') {
+			++count_buf;
+			return (SUCCESS);
+		}
+	}
+	if (res == -1)
+		return (FAIL);
+	return (SUCCESS);
 }
 
 char	*get_next_line(int fd)
 {
-	static char buffer[READ_SIZE] = "";
-	static int readed = 0;
-	static int size = 0;
-	char *line = NULL;
+	static char	buffer[READ_SIZE + 1] = { '\0' };
+	int		current_file = 0;
+	char		*ret = NULL;
+	int		status = 0;
 
-	if (size == readed) {
-		readed = read(fd, buffer, READ_SIZE);
-		if (readed == 0)
-			return (NULL);
-		size = 0;
+	if ((status = begin_read(buffer, &current_file, fd, &ret)) == FAIL)
+		return (ERROR);
+	else if (status == CONTINUE)
+		return (ret);
+	if (current_file == 0) {
+		if (ret[0] == '\0') {
+			free(ret);
+			return (ERROR);
+		}
 	}
-	line = make_line(fd, buffer, &size, &readed);
-	return (line);
+	else if (current_file == -1) {
+		if (ret == NULL)
+			return (ERROR);
+	}
+	return (ret);
 }
