8426eec9b4b7b7c207a5e7348239bba144f28c42
Justine Duhieu <justine.duhieu@epitech.eu>
2018-03-12 18:09:31 +0100

    include with define for error

---
+++ include/tools_lemin.h
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2018
+** lemin
+** File description:
+** tools lemin
+*/
+
+#ifndef	TOOLS_LEMIN_H_
+#define	TOOLS_LEMIN_H_
+
+#define	SUCCESS			0
+#define	ERROR			84
+#define	ERR_NULL		NULL
+
+#endif		/* TOOLS_LEMIN_H_ */
