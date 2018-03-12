950b4e602237cac241396ad35e51cfe8e0a4d873
Joubert Sacha <Joubert_S@desktop-ps7pru0.home>
2018-03-12 18:04:10 +0100

    add init_map

---
+++ src/init_map.c
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2018
+** lemin
+** File description:
+** init_map.c
+*/
+
+#include "get_next_line.h"
+#include "struct.h"
+
+int	init_map(char **file_name, room_s *room)
+{
+	
+}
