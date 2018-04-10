47e2f394720b0bbd6150eb3eb159013d87b59215
Justine Duhieu <justine.duhieu@epitech.eu>
2018-04-10 16:42:50 +0200

    verify if there is a great tunnel

--- src/init/counter.c
+++ src/init/counter.c
@@ -26,6 +26,26 @@ int	counter_space(char *str)
 	return (space);
 }
 
+static int	verify_tunnel(char *str)
+{
+	unsigned int	count = 0;
+	unsigned int	count2 = 0;
+
+	while (str[count] != '-') {
+		count++;
+	}
+	if (count == 0)
+		return (-1);
+	count++;
+	while (str[count] != '\0') {
+		count++;
+		count2++;
+	}
+	if (count2 == 0)
+		return (-1);
+	return (0);
+}
+
 int	counter_hyphen(char *str)
 {
 	int hyphen = 0;
@@ -34,8 +54,11 @@ int	counter_hyphen(char *str)
 	if (str[0] == '#')
 		return (-1);
 	while (str[index] != '\0') {
-		if (str[index] == '-')
+		if (str[index] == '-') {
+			if (verify_tunnel(str) == -1)
+				return (-1);
 			hyphen++;
+		}
 		index++;
 	}
 	return (hyphen);
