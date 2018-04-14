a99559671cb7e11fb838a9613fbbe4bce517d10d
Camilleri Nathan <nathan.camilleri@epitech.eu>
2018-04-14 16:27:52 +0100

    regular norm sacha fucntion

--- src/find_way/find_way.c
+++ src/find_way/find_way.c
@@ -40,14 +40,7 @@ room_s	*link_path_way(path_s *path, room_s *room, info_s *info)
 			break;
 		}
 		else if (room->next[path->array] == NULL) {
-			path->over = add_array(room, path->over);
-			if (nb_array(path->prev) == 0) {
-				room = path->prev[nb_array(path->prev)];
-				path->prev[nb_array(path->prev)] = NULL;
-			} else {
-				room = path->prev[nb_array(path->prev) - 1];
-				path->prev[nb_array(path->prev) - 1] = NULL;
-			}
+			link_path_way_3(path, room);
 			break;
 		}
 		path->array += 1;
--- src/find_way/find_way_2.c
+++ src/find_way/find_way_2.c
@@ -19,3 +19,15 @@ room_s	*link_path_way_2(path_s *path, info_s *info)
 			info->exit = -1;
 	return (0);
 }
+
+void 	link_path_way_3(path_s *path, room_s *room)
+{
+	path->over = add_array(room, path->over);
+	if (nb_array(path->prev) == 0) {
+		room = path->prev[nb_array(path->prev)];
+		path->prev[nb_array(path->prev)] = NULL;
+	} else {
+		room = path->prev[nb_array(path->prev) - 1];
+		path->prev[nb_array(path->prev) - 1] = NULL;
+	}
+}
